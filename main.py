39c22c36cda8fd16f5803686646c57b2559cd646
Maria Babina <babina.mariyka@gmail.com>
2024-04-28 21:07:23 +0300

    Методы CREATE, EDIT, DELETE для заметок

--- main.py
+++ main.py
@@ -18,3 +18,35 @@ def save_note(note):
     filename = get_file_name_for(note['id'])
     with open(filename, 'w') as f:
         json.dump(note, f)
+
+
+def create_note(note_id, title, body):
+    note = {
+        "id": note_id,
+        "title": title,
+        "body": body,
+        "created": str(datetime.datetime.now()),
+        "updated": None
+    }
+    return note
+
+
+def edit_note(note_id, new_title, new_body):
+    filename = get_file_name_for(note_id)
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            note = json.load(f)
+        note['title'] = new_title
+        note['body'] = new_body
+        note['updated'] = str(datetime.datetime.now())
+        save_note(note)
+    else:
+        print("WARNING: Note not found.")
+    
+
+def delete_note(note_id):
+    filename = get_file_name_for(note_id)
+    if os.path.exists(filename):
+        os.remove(filename)
+    else:
+        print("WARNING: Note not found.")
