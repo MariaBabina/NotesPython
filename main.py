48bf3b75c52e5ea6f9b98c143011ed74b16fc0cd
Maria Babina <babina.mariyka@gmail.com>
2024-04-28 21:11:22 +0300

    Добавлен основной метод программы с меню

--- main.py
+++ main.py
@@ -59,3 +59,53 @@ def read_notes():
             note = json.load(f)
             notes.append(note)
     return notes
+
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("[1]-Create   [2]-Read all notes   [3]-Edit   [4]-Delete   [5]-Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            note_id = input("Enter note ID: ")
+            # check note already exists
+            if note_exists(note_id):
+                print("WARNING: Note with such ID already exists! Use 'Edit' operation.")
+                continue
+            title = input("Enter note title: ")
+            body = input("Enter note body: ")
+            note = create_note(note_id, title, body)
+            save_note(note)
+            print("Note saved successfully.")
+
+        elif choice == "2":
+            print("All notes:")
+            print("----------------------------------------------------------------------------------------------------")
+            notes = read_notes()
+            for note in notes:
+                print(f"#{note['id']}: {note['title']} :::: {note['body']}, (created/updated : {note['created']}/{note['updated']})")
+            print("----------------------------------------------------------------------------------------------------")    
+
+        elif choice == "3":
+            note_id = input("Enter ID of note to edit: ")
+            new_title = input("Enter new title: ")
+            new_body = input("Enter new body: ")
+            edit_note(note_id, new_title, new_body)
+            print("Note edited successfully.")
+
+        elif choice == "4":
+            note_id = input("Enter ID of note to delete: ")
+            delete_note(note_id)
+            print("Note deleted successfully.")
+
+        elif choice == "5":
+            print("Exiting...")
+            break
+
+        else:
+            print("WARNING: Invalid choice. Please enter a number from 1 to 5.")
+
+
+if __name__ == "__main__":
+    main()
